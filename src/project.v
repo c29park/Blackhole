fd88a462c3f1cc320dc4d92a4ed2b97a96385eea
Saubaan Hasan <106204098+SaubaanH@users.noreply.github.com>
2025-12-04 00:11:11 -0500

    Clean up code

--- src/project.v
+++ src/project.v
@@ -194,35 +194,6 @@ module tt_um_vga_example(
 
 endmodule
 
-// Wrapper to satisfy testbench expectation of tt_um_example
-module tt_um_example(
-  input  wire [7:0] ui_in,
-  output wire [7:0] uo_out,
-  input  wire [7:0] uio_in,
-  output wire [7:0] uio_out,
-  output wire [7:0] uio_oe,
-  input  wire       ena,
-  input  wire       clk,
-  input  wire       rst_n
-`ifdef GL_TEST
- ,input  wire       VPWR,
-  input  wire       VGND
-`endif
-);
-
-  tt_um_vga_example core (
-    .ui_in  (ui_in),
-    .uo_out (uo_out),
-    .uio_in (uio_in),
-    .uio_out(uio_out),
-    .uio_oe (uio_oe),
-    .ena    (ena),
-    .clk    (clk),
-    .rst_n  (rst_n)
-  );
-
-endmodule
-
 // -------------------------------------------------------
 // VGA Timing Generator
 // -------------------------------------------------------
